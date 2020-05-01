bead40cc876a9d9b407602d29e6088981d2af9fd
Yury <y.petukhou@exadel.com>
2020-05-02 01:57:04 +0300

    crud

---
+++ index.js
@@ -0,0 +1,55 @@
+const expess = require('express');
+const mongo = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const ProductSchema = new Schema({
+    img: String,
+    type: String,
+    title: String,
+    description: String,
+    cost: Number
+});
+const Product = mongoose.model("product", ProductSchema);
+const app =  expess();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('', (req, res) => {
+    const card = new Product({...req.body});
+    card.save()
+        .then(item => {
+            console.log({item});
+            res.send(item)
+        })
+        .catch(err => console.log({err}))
+
+});
+// will find all items
+app.get('/catalog', (req, res) => {
+    Product.find({}, (err, items) => {
+        res.send(items)
+    })
+});
+
+app.get("/catalog/:type", (req, res) => {
+    console.log(req.params)
+     Product.find({...req.params}, (err, item) => {
+        if (err) {
+            res.send("Smth went wrong");
+            return
+        };
+        if(!item) {
+            res.send("Sorry product is empty ")
+            return
+        }
+         res.send(item)
+     } )
+})
+mongoose.connect('mongodb+srv://nerds:132445@cluster0-enzzy.mongodb.net/test?retryWrites=true&w=majority',)
+    .then(client => console.log('DATABASE CONNECTED'))
+    .then(data => app.listen(300, () => console.log('SERVER STARTED ON 300')))
+    .catch(err => console.log(err));
